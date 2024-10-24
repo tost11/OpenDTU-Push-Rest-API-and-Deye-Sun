a57b5517e9ea647d1e5eaf4cafcad6bb1efe442b
lukas <tost>
2024-10-24 19:49:12 +0200

    impvored improts

--- include/TostHandle.h
+++ include/TostHandle.h
@@ -2,14 +2,13 @@
 #pragma once
 
 #include <TimeoutHelper.h>
-#include <iostream>
-#include <Hoymiles.h>
 #include "Configuration.h"
 #include <TaskSchedulerDeclarations.h>
 #include <BaseInverter.h>
-#include <ArduinoJson.h>
-#include <future>
-#include <HTTPClient.h>
+#include <queue>
+#include <thread>
+
+class HTTPClient;
 
 struct http_requests_to_send{
     http_requests_to_send(uint32_t timestamp):
@@ -44,23 +43,6 @@ private:
 
     const long TIMER_CLEANUP = 1000 * 60 * 5;
 
-    FieldId_t _publishFields[14] = {
-            FLD_UDC,
-            FLD_IDC,
-            FLD_PDC,
-            FLD_YD,
-            FLD_YT,
-            FLD_UAC,
-            FLD_IAC,
-            FLD_PAC,
-            FLD_F,
-            FLD_T,
-            FLD_PF,
-            FLD_EFF,
-            FLD_IRR,
-            FLD_Q
-        };
-
     std::string generateUniqueId(const BaseInverterClass & inv);
 
     void handleResponse();
@@ -74,7 +56,6 @@ public:
     unsigned long getLastSuccessfullyTimestamp()const{return lastSuccessfullyTimestamp;}
     int getLastErrorStatusCode()const{return lastErrorStatusCode;}
     const std::string & getLastErrorMessage()const{return lastErrorMessage;}
-
 };
 
 extern TostHandleClass TostHandle;
