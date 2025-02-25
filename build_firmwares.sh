87ed9ad38c2f80306b25826b08fabf6507f4ab6a
lukas <tost>
2025-02-26 00:15:46 +0100

    fixed build script

--- build_firmwares.sh
+++ build_firmwares.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-TARGET_FOLDER=$(realpath ~/Schreibtisch/opendtu/)
+TARGET_FOLDER=$(realpath ~/Schreibtisch/opendtu)
 BUILD_COMMAND='~/.platformio/penv/bin/platformio run'
 
 echo "Target folder is: $TARGET_FOLDER"
@@ -13,15 +13,16 @@ createBuild() {
   rm -Rf .pio/build
   npm run build --prefix webapp
   eval $BUILD_COMMAND
-  cp .pio/build/generic_esp32/firmware.bin $TARGET_FOLDER$2_esp32dev_firmware.bin
+  echo "Build: $2_esp32dev_firmware.bin copy to: $TARGET_FOLDER"
+  cp .pio/build/generic_esp32/firmware.bin $TARGET_FOLDER/$2_esp32dev_firmware.bin
 }
 
 createBuild "feature/more-manufacturers" "more_manufacturers"
-createBuild "feature/deye-sun" "deye"
-createBuild "feature/hoymiles-HMS-W-2T" "HMS-XXXW-XT"
-createBuild "feature/servo" "servo"
-createBuild "feature/default-start-date" "starttime"
-createBuild "feature/push-rest-api" "rest"
-createBuild "combine/rest-more-manufacturers" "rest_more_manufacturers"
-createBuild "combine/rest-no-nrf-hoymiles" "rest_no_nrf"
-createBuild "combine/all" "all_in_one"
+#createBuild "feature/deye-sun" "deye"
+#createBuild "feature/hoymiles-HMS-W-2T" "HMS-XXXW-XT"
+#createBuild "feature/servo" "servo"
+#createBuild "feature/default-start-date" "starttime"
+#createBuild "feature/push-rest-api" "rest"
+#createBuild "combine/rest-more-manufacturers" "rest_more_manufacturers"
+#createBuild "combine/rest-no-nrf-hoymiles" "rest_no_nrf"
+#createBuild "combine/all" "all_in_one"
