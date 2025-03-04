8f05e0976d6f441aff7ca1f6979e85f17cec858e
lukas <tost>
2025-03-04 14:41:48 +0100

    changed some data fetching timers for hoymiles-w inverters

--- lib/InverterLib/src/inverter/BaseNetworkInverter.h
+++ lib/InverterLib/src/inverter/BaseNetworkInverter.h
@@ -87,12 +87,13 @@ protected:
                 if(found != apMacsAndIps->end()){
                     MessageOutput.printfDebug("Found ip for mac is: %s\n",found->second.c_str());
                     if(found->second != "0.0.0.0" && !found->second.empty()){
-                        _macToIpResolverTimer.set(TIMER_FAILED_MAC_IP_RESOLUTION);
-                        MessageOutput.printf("Resolved Mac to ip: %s\n", _resolvedIpByMacAdress->c_str());
+                        MessageOutput.printf("Resolved Mac to ip: %s\n", found->second.c_str());
                         if(_resolvedIpByMacAdress == nullptr || found->second != *_resolvedIpByMacAdress){
                             _resolvedIpByMacAdress = std::make_unique<std::string>(found->second);
+                            _macToIpResolverTimer.set(TIMER_SUCCES_MAC_IP_RESOLUTION);
                             return true;
                         }
+                        _macToIpResolverTimer.set(TIMER_FAILED_MAC_IP_RESOLUTION);
                         MessageOutput.printfDebug("Resolved Ip is same as before\n");
                         return false;
                     }
