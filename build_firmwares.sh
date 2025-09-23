909f4fe107ae44e5f61384b8981c618fb6b5169c
lukas <tost>
2025-09-23 10:08:07 +0200

    build script

---
+++ build_firmwares.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+TARGET_FOLDER=$(realpath ~/Schreibtisch/opendtu)
+BUILD_COMMAND='~/.platformio/penv/bin/platformio run'
+BUILD_COMMAND_16='~/.platformio/penv/bin/platformio run -e generic_esp32_16mb_psram'
+
+echo "Target folder is: $TARGET_FOLDER"
+
+createBuild() {
+  git checkout $1
+  #clear existing old build stuff (some times not clear build done)
+  rm -Rf .pio/build
+  yarn --cwd webapp run build
+
+  eval $BUILD_COMMAND
+  echo "Build Generic: $2_esp32dev_firmware.bin copy to: $TARGET_FOLDER"
+  mkdir -p $TARGET_FOLDER/generic_esp32
+  cp .pio/build/generic_esp32/firmware.bin $TARGET_FOLDER/generic_esp32/$2_esp32dev_firmware.bin
+
+  eval $BUILD_COMMAND_16
+  echo "Build 16MB psram: $2_esp32dev_firmware.bin copy to: $TARGET_FOLDER"
+  mkdir -p $TARGET_FOLDER/generic_esp32_16mb_psram
+  cp .pio/build/generic_esp32_16mb_psram/firmware.bin $TARGET_FOLDER/generic_esp32_16mb_psram/$2_esp32dev_firmware.bin
+}
+
+createBuild "feature/more-manufacturers" "more_manufacturers"
+createBuild "feature/deye-sun" "deye"
+createBuild "feature/hoymiles-HMS-W-2T" "HMS-XXXW-XT"
+createBuild "feature/servo" "servo"
+createBuild "feature/default-start-date" "starttime"
+createBuild "feature/push-rest-api" "rest"
+createBuild "combine/rest-more-manufacturers" "rest_more_manufacturers"
+createBuild "combine/rest-no-nrf-hoymiles" "rest_no_nrf"
+createBuild "combine/all" "all_in_one"
